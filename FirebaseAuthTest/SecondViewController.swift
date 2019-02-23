42e9e97b649a16f7325f50fc805493fffba1caf8
hnishim <nishimiyahirotaka@gmail.com>
2019-02-23 16:27:42 +0900

    Add missing files

---
+++ FirebaseAuthTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FirebaseAuthTest
+//
+//  Created by Hiro Nishimiya on 2019/02/23.
+//  Copyright © 2019 Hiro Nishimiya. All rights reserved.
+//
+
+import UIKit
+import FirebaseUI
+
+class SecondViewController: UIViewController, FUIAuthDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("second")
+    }
+}
--- FirebaseAuthTest/ViewController.swift
+++ FirebaseAuthTest/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import FirebaseUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FUIAuthDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        
+        let authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI!.delegate = self
+        
+        let authViewController = authUI!.authViewController()
+        present(authViewController, animated: true, completion: nil)
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        // handle user and error as necessary
+    }
 }
-
