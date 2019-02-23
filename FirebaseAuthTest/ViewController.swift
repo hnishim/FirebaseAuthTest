6dc5778f1ecc254f5c713c5552daa61b554522b0
hnishim <nishimiyahirotaka@gmail.com>
2019-02-23 11:22:00 +0900

    Initial Commit

---
+++ FirebaseAuthTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseAuthTest
+//
+//  Created by Hiro Nishimiya on 2019/02/23.
+//  Copyright © 2019 Hiro Nishimiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
